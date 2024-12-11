1fc8b75cf2b406f44aad6a869333a5b3c0715077
gerardjr42 <ggarciajr12@gmail.com>
2024-12-11 02:34:39 -0500

    Enhance route.ts: - Imported groq to be able to use the LLM - Import cheerio and puppeteer for web scraping - Implement scrapeWebpage function to fetch and parse HTML content - Remove unneeded elements and script tags - Improve error handling for better debugging

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,17 +1,10 @@
-// TODO: Implement the chat API with Groq and web scraping with Cheerio and Puppeteer
 // Refer to the Next.js Docs on how to read the Request body: https://nextjs.org/docs/app/building-your-application/routing/route-handlers
 // Refer to the Groq SDK here on how to use an LLM: https://www.npmjs.com/package/groq-sdk
 // Refer to the Cheerio docs here on how to parse HTML: https://cheerio.js.org/docs/basics/loading
 // Refer to Puppeteer docs here: https://pptr.dev/guides/what-is-puppeteer
 
-import { NextResponse } from "next/server";
+import { Groq } from "groq-sdk";
 
-export async function GET() {
-  //testing
-  return NextResponse.json({ message: "Hello, World!" });
-}
-
-export async function POST(request: Request) {
-  try {
-  } catch (error) {}
-}
+const client = new Groq({
+  apiKey: process.env["GROQ_API_KEY"],
+});
